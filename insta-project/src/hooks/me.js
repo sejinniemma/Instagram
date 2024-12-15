73a6db8eaa29c5b7d96a2a42bded9ee4f40ef1dd
sejinniemma <jsj0471@naver.com>
2024-12-15 20:42:14 +0900

    1215_004
    - 성능개선 : useCallback

--- insta-project/src/hooks/me.js
+++ insta-project/src/hooks/me.js
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import useSWR from 'swr';
 
 async function updateBookmark(postId, bookmark) {
@@ -10,21 +11,24 @@ async function updateBookmark(postId, bookmark) {
 export default function useMe() {
   const { data: user, isLoading, error, mutate } = useSWR('/api/me');
 
-  const setBookMark = (postId, bookmark) => {
-    const bookmarks = user.bookmarks;
-    const newUser = {
-      ...user,
-      bookmarks: bookmark
-        ? [...bookmarks, postId]
-        : bookmarks.filter((bookmark) => bookmark !== postId),
-    };
+  const setBookMark = useCallback(
+    (postId, bookmark) => {
+      const bookmarks = user.bookmarks;
+      const newUser = {
+        ...user,
+        bookmarks: bookmark
+          ? [...bookmarks, postId]
+          : bookmarks.filter((bookmark) => bookmark !== postId),
+      };
 
-    return mutate(updateBookmark(postId, bookmark), {
-      optimisticData: newUser,
-      populateCache: false, // updateLike에서 반환된 데이터를 사용하지 않겠다 -> newPosts로 대체할거
-      revalidate: false, // 백에서 받아온 데이터를 다시 클라이언트에 업데이트 시키겠다. (불필요)
-      rollbackOnError: true,
-    });
-  };
+      return mutate(updateBookmark(postId, bookmark), {
+        optimisticData: newUser,
+        populateCache: false, // updateLike에서 반환된 데이터를 사용하지 않겠다 -> newPosts로 대체할거
+        revalidate: false, // 백에서 받아온 데이터를 다시 클라이언트에 업데이트 시키겠다. (불필요)
+        rollbackOnError: true,
+      });
+    },
+    [user, mutate]
+  );
   return { user, setBookMark };
 }
--- insta-project/src/hooks/post.js
+++ insta-project/src/hooks/post.js
@@ -1,4 +1,5 @@
-import useSWR from 'swr';
+import { useCallback } from 'react';
+import useSWR, { useSWRConfig } from 'swr';
 
 async function addComment(id, comment) {
   return fetch('/api/comments', {
@@ -15,19 +16,23 @@ export default function useFullPost(postId) {
     mutate,
   } = useSWR(`/api/posts/${postId}`);
 
-  const postComment = (comment) => {
-    if (!post) return;
-    const newPost = {
-      ...post,
-      comments: [...post.comments, comment],
-    };
+  const { mutate: gloablMutate } = useSWRConfig();
+  const postComment = useCallback(
+    (comment) => {
+      if (!post) return;
+      const newPost = {
+        ...post,
+        comments: [...post.comments, comment],
+      };
 
-    return mutate(addComment(post.id, comment.comment), {
-      optimisticData: newPost,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    });
-  };
+      return mutate(addComment(post.id, comment.comment), {
+        optimisticData: newPost,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      }).then(() => gloablMutate('/api/posts'));
+    },
+    [mutate, gloablMutate, post]
+  );
   return { post, isLoading, error, postComment };
 }
